8a9ed4b3eb8227485a23beda99c3effad5e98b87
Roufox <61273311+Rouf0x@users.noreply.github.com>
2024-03-01 20:25:00 +0100

    Create beep.lua
    Beep command to play sounds from a speaker.

---
+++ OS/programs/beep.lua
@@ -0,0 +1,22 @@
+args = {...}
+
+local sound = args[1]
+
+if peripheral.find("speaker") then
+    if sound then
+        if sound == "stop" then
+            print("Stopped.")
+        else
+            peripheral.find("speaker").playSound(sound)
+            print("Played '"..sound.."'")
+        end
+    else
+        print("Usage: beep [sound]/stop")
+        print("Played 'entity.arrow.hit_player'")
+        peripheral.find("speaker").playSound("entity.arrow.hit_player") 
+    end
+else
+    term.setTextColor(colors.red)
+    print("A speaker is required for this command!")
+    term.setTextColor(colors.white)
+end
