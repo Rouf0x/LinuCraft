641b169410e0798a3087e421c3cccc652b4965f9
Roufox <61273311+Rouf0x@users.noreply.github.com>
2024-03-01 20:24:27 +0100

    Create about.lua

---
+++ OS/programs/about.lua
@@ -0,0 +1,7 @@
+term.setTextColor(colors.yellow)
+print("LinuCraft 6.1.0-2712 #1 SMP PREEMPT Debian 1:6.1.73-1+rpt1 (2024-01-25) aarch64")
+print("")
+print("The programs included with the Debian GNU/LinuCraft system are free software;")
+print("")
+print("Debian GNU/LinuCraft comes with ABSOLUTELY NO WARRANTY, to the extentpermitted by applicable law.")
+term.setTextColor(colors.white)
